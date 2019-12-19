edb9a1c5360180786bcbf749c85146bcfe5afd49
Jimmy Nguyen <jimmynguyen@hotmail.co.uk>
2019-12-19 20:29:34 +0000

    Made code more pythonic

--- strong_password_detector.py
+++ strong_password_detector.py
@@ -4,26 +4,25 @@
 import re
 
 def detector():
-
-    password = input("Please enter password you wish to use here: ")
-    character_regex = re.compile(r"\w{8,15}$")
-    lowercase_regex = re.compile(r"[a-z]")
-    uppercase_regex = re.compile(r"[A-Z]")
-    number_regex    = re.compile(r"[0-9]")
-
     while True:
+        password = input("Please enter password you wish to use here: ")
+        character_regex = re.compile(r"\w{8,15}$")
+        lowercase_regex = re.compile(r"[a-z]")
+        uppercase_regex = re.compile(r"[A-Z]")
+        number_regex    = re.compile(r"[0-9]")
+
         if character_regex.search(password) == None:
             print("Password needs to be at least 8-15 characters long with at least one uppercase, one lower case, and one numerical value")
-            detector()
+            continue
         elif lowercase_regex.search(password) == None:
             print("Password needs to be at least 8-15 characters long with at least one uppercase, one lower case, and one numerical value")
-            detector()
+            continue
         elif uppercase_regex.search(password) == None:
             print("Password needs to be at least 8-15 characters long with at least one uppercase, one lower case, and one numerical value")
-            detector()
+            continue
         elif number_regex.search(password) == None:
             print("Password needs to be at least 8-15 characters long with at least one uppercase, one lower case, and one numerical value")
-            detector()
+            continue
         else:
             print("Password is very secure.")
             break
