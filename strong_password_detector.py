a6573cd9d7b3573f7d668581238d546134af28f4
Jimmy Nguyen <jimmynguyen@hotmail.co.uk>
2019-12-19 22:06:56 +0000

    cleaned up code

--- strong_password_detector.py
+++ strong_password_detector.py
@@ -2,26 +2,27 @@
 # strong_password_detector.py - Will make sure password is eight characters long with at least ONE lowercase,
 # ONE uppercase and ONE numerical value.
 import re
+
 print("Password needs to be at least 8-15 characters long with at least one uppercase, one lower case,\nand one numerical value to be secure")
+
 def detector():
     while True:
         password = input("Please enter password you wish to use here: ")
-        character_regex = re.compile(r"\w{8,15}$")
-        lowercase_regex = re.compile(r"[a-z]")
-        uppercase_regex = re.compile(r"[A-Z]")
-        number_regex    = re.compile(r"[0-9]")
 
-        if character_regex.search(password) == None:
-            print("Password needs to be at least 8-15 characters long.")
-            continue
-        elif lowercase_regex.search(password) == None:
-            print("Password needs at least one lowercase character.")
-            continue
-        elif uppercase_regex.search(password) == None:
-            print("Password needs at least one uppercase character.")
-            continue
-        elif number_regex.search(password) == None:
-            print("Password needs at least one numerical value.")
+        password_regex = re.compile(r"""
+        (?=.*[a-z])    # at least one lowercase character
+        (?=.*[A-Z])    # at least one uppercase character
+        (?=.*[0-9])    # at least one digit
+        .{8,}          # at least eight characters long  
+        """, re.VERBOSE)
+
+        if password_regex.search(password) == None:
+            print("""Needs to be:\n
+            eight characters long,\n
+            at least one lowercase character,\n
+            at least one uppercase character,\n
+            and at least one number.
+            """)
             continue
         else:
             print("Password is very secure.")
